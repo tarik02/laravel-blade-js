ac9e99f2921957b57b60e4b964bed38050697e15
Tarik02 <Taras.Fomin@gmail.com>
2019-07-14 12:41:40 +0300

    Add trimAsyncIterable

---
+++ src/runtime/trimAsyncIterable.ts
@@ -0,0 +1,34 @@
+export const trimAsyncIterable = async function *(iteratee: AsyncIterable<string>): AsyncIterable<string> {
+  const iterator = iteratee[Symbol.asyncIterator]();
+
+  let tmp: string;
+
+  // Trim left
+  do {
+    const item = await iterator.next();
+    if (item.done) {
+      return;
+    }
+
+    tmp = item.value.trimLeft();
+  } while (tmp.length === 0);
+
+  // Trim right
+  let whitespace = '';
+  while (true) {
+    const trimmed = tmp.trimRight();
+    if (trimmed.length === 0) {
+      whitespace += tmp;
+    } else {
+      yield whitespace;
+      yield trimmed;
+      whitespace = tmp.substring(trimmed.length);
+    }
+
+    const item = await iterator.next();
+    if (item.done) {
+      return;
+    }
+    tmp = item.value;
+  }
+};
